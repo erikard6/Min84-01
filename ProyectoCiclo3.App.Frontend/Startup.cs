b327d34cfed0cf4336a4b68b8b09fa4b0f9fc516
mario.sanchez.interacpedia <mario.sanchez@talentpitch.co>
2022-09-19 21:46:09 -0500

    auth

--- ProyectoCiclo3.App.Frontend/Startup.cs
+++ ProyectoCiclo3.App.Frontend/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ProyectoCiclo3.App.Persistencia.AppRepositorios;
+using Microsoft.AspNetCore.Authentication;
 
 namespace ProyectoCiclo3.App.Frontend
 {
@@ -28,6 +29,7 @@ namespace ProyectoCiclo3.App.Frontend
             services.AddSingleton<RepositorioAeropuertos, RepositorioAeropuertos>();
             // services.AddSingleton<RepositorioAviones, RepositorioAviones>();
             services.AddSingleton<RepositorioRutas, RepositorioRutas>();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +50,7 @@ namespace ProyectoCiclo3.App.Frontend
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
